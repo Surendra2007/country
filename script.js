260087c98fe14f93794bb770acb1a6fa6b626b46
Surendra Dhaka <saabchoudhary350@gmail.com>
2024-12-20 19:46:45 +0530

    changejs

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let api="https://restcountries.com/v3.1/all";
 let main=document.querySelector("main");
 let input=document.querySelector("input");
-console.log(input);
+let select=document.querySelector("select");
+
 
 
 
@@ -16,6 +17,10 @@ fetch(api)
             showData(filteredArray);
       
           });
+          select.addEventListener("change",()=>{
+              let filtereddata = json.filter((element)=> element.region === select.value)
+              showData(filtereddata)
+              });
         })
     //   // Function to show data
 
@@ -31,18 +36,19 @@ fetch(api)
                 <h2>${element.name.common}</h2>
             <p><span class="bold">Population</span> : ${element.population}</p>
             <p><span class="bold">Region</span> : ${element.region}</p>
-            <p><span class="bold">Capital</span> : ${element.capital[0]}</p>
+            <p><span class="bold">Capital</span> : ${element.capital}</p>
           </div>
         `
       main.appendChild(div);
     });
   }
 
-let header=document.getElementById("head")
+
 let mode=document.getElementById("mode");
-let body=document.body;
 mode.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
+    input.classList.toggle("dark-mode");
+    select.classList.toggle("dark-mode");
     document.querySelector("header").classList.toggle("dark-mode");
     document.querySelectorAll(".box").forEach((box) => {
         box.classList.toggle("dark-mode");
